47ab149c47bbc1476f64bb99bf334141be6a4d6d
Pruthvi Menpara <pruthvimenpara@Kajal-Gajjar.local>
2024-03-22 12:06:08 +0530

    fix: custom extension sso router datatype

--- custom/bundles/localeyz/src/routes/endpoints/sso.ts
+++ custom/bundles/localeyz/src/routes/endpoints/sso.ts
@@ -1,4 +1,3 @@
-import { Request, Response, Router } from 'express' // Assuming you're using Express for routing
 import { userLogin } from '../../controllers/sso'
 import { ControllerOptions } from '../../utils/helper'
 
@@ -6,7 +5,15 @@ export default {
   // Unique identifier for this route handler
   id: 'sso',
   // Handler function responsible for setting up the route
-  handler: (router: Router, { services, getSchema }: ControllerOptions) => {
+  handler: (
+    router: {
+      post: (
+        arg0: string,
+        arg1: (req: Request, res: Response) => Promise<void>
+      ) => void
+    },
+    { services, getSchema }: ControllerOptions
+  ) => {
     // Define the route for handling user login
     router.post('/login', async (req: Request, res: Response) => {
       // Call the userLogin function from the SSO controller
